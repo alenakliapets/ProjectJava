1e24194a7dc7ae531852ea8d6e666acd560afb1e
elenaklepec <ckarlettt@rambler.ru>
2021-09-21 20:03:21 +0300

    Changes

--- src/Hello2.java
+++
@@ -1,8 +0,0 @@
-public class Hello2 {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("Hi");
-        System.out.println("Hello2 World");
-        System.out.println("Вася проверь задачку");
-    }
-}
